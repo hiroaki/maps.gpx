46e30f6090ea2d744e8d3ba50ea025ddd0bfa762
WATANABE Hiroaki <hwat@mac.com>
2015-04-04 17:20:36 +0900

    bump up version 2.1.0

--- gpx-casual-viewer.js
+++ gpx-casual-viewer.js
@@ -9,7 +9,7 @@ function GPXCasualViewer() {
 }
 
 // constants, do not change these value
-GPXCasualViewer.VERSION   = '2.0.0';
+GPXCasualViewer.VERSION   = '2.1.0';
 GPXCasualViewer.GPX_TYPE  = 'gpxType';
 GPXCasualViewer.WPT_TYPE  = 'wptType';
 GPXCasualViewer.RTE_TYPE  = 'rteType';
