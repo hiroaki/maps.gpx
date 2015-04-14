22108bcab14b0460150409f8022fd53810c977b7
WATANABE Hiroaki <hwat@mac.com>
2015-04-15 02:05:37 +0900

    refs #9 add experimental feature, search the nearest trkpt to the time of the image

--- plugin-exif-markers/loader.js
+++ plugin-exif-markers/loader.js
@@ -4,25 +4,41 @@ GPXCasualViewer.plugin.EXIFMarkers = {
     this.register('onReadEXIF', (function (key, values){
 
       GPXCasualViewer.plugin.EXIFMarkers.pts.push({lat: values.latlng.lat(), lon: values.latlng.lng()});
-      if( 1 < GPXCasualViewer.plugin.EXIFMarkers.pts.length ){
+      if ( 1 < GPXCasualViewer.plugin.EXIFMarkers.pts.length ) {
         var bounds = GPXCasualViewer.boundsOf(GPXCasualViewer.plugin.EXIFMarkers.pts);
         this.map.fitBounds(GPXCasualViewer.createLatlngbounds(bounds));
-      }else{
+      } else {
         this.map.panTo(values.latlng);
       }
-      var contents = '<div><strong>'+ values.exif['DateTimeOriginal'] +' - '+ key + '</strong></div><hr/>'+
-                     '<div><img class="info-window" src="'+ values.url +'"/></div>';
-      var marker = new google.maps.Marker({
-        position: values.latlng,
-        draggable: true,
-        icon: new google.maps.MarkerImage('plugin-exif-markers/photo.png', new google.maps.Size(32,37))
-      });
-      marker.addListener('click', function(mouseevent) {
-        new google.maps.InfoWindow({
-          content: contents
-          }).open(this.map, this);
-      });
-      marker.setMap(this.map);
+
+      var pinpoint = null;
+      if ( 0 == values.alternatives.length ) {
+        pinpoint = values.latlng;
+      } else {
+        for ( var i = 0, l = values.alternatives.length; i < l; ++i ) {
+          if ( window.confirm('Are you sure the image is on "'+ values.alternatives[i]['key'] +'" ?') ) {
+            pinpoint = values.alternatives[i].latlng;
+            break;
+          }
+        }
+      }
+      if ( ! pinpoint ) {
+        window.alert('Could not detect coordinate of the image "'+ key +'"')
+      } else {
+        var contents = '<div><strong>'+ values.exif['DateTimeOriginal'] +' - '+ key + '</strong></div><hr/>'+
+                       '<div><img class="info-window" src="'+ values.url +'"/></div>';
+        var marker = new google.maps.Marker({
+          position: values.latlng,
+          draggable: true,
+          icon: new google.maps.MarkerImage('plugin-exif-markers/photo.png', new google.maps.Size(32,37))
+        });
+        marker.addListener('click', function(mouseevent) {
+          new google.maps.InfoWindow({
+            content: contents
+            }).open(this.map, this);
+        });
+        marker.setMap(this.map);
+      }
     }).bind(this));
 
   }
