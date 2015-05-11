dc1301d4341a6d5388fce09a29dd6a7c89505ee1
WATANABE Hiroaki <hwat@mac.com>
2015-05-12 01:26:29 +0900

    Add plugin DescImage

---
+++ plugins/DescImage/README.txt
@@ -0,0 +1,3 @@
+The icon images in this folder, are created by:
+
+  Maps Icons Collection https://mapicons.mapsmarker.com
---
+++ plugins/DescImage/loader.js
@@ -0,0 +1,32 @@
+GPXCasualViewer.plugin.DescImage = {
+  path: null,
+  callback: function() {
+    GPXCasualViewer.plugin.detectPathOfPlugin('DescImage');
+
+    this.register('onCreateMarker', (function(marker) {
+      var desc = marker.getSource().desc;
+      if (
+        ( ! new RegExp('[\x0d\x0a]').test(desc) && new RegExp('\.jpe?g$', 'i').test(desc) )
+      ||
+        new RegExp('^data:image/jpeg;base64,[+/=0-9A-Za-z\x0d\x0a]+$').test(desc)
+      ||
+        new RegExp('^blob:(https?(:|%3A)//[-_.(:|%3A)0-9A-Za-z]+?|file(:|%3A)//)/[-0-9A-Za-z]+$').test(desc)
+      ||
+        new RegExp('^blob:null/[-0-9A-Za-z]+$').test(desc)
+      ) {
+        google.maps.event.addListener(marker, 'click', function(mouseevent) {
+          if ( ! this._infowindow ) {
+            this._infowindow = new google.maps.InfoWindow({
+              content: ['<div><img class="info-window" src="',desc,'"/></div>'].join('')
+              });
+          }
+          this._infowindow.open(this.getMap(), this);
+        });
+        marker.setIcon(
+          new google.maps.MarkerImage(
+            GPXCasualViewer.plugin.DescImage.path +'photo.png',
+            new google.maps.Size(32,37)));
+      }
+    }).bind(this));
+  }
+};
