1146846d88df523f598312b8b1226c272cf46c99
WATANABE Hiroaki <hwat@mac.com>
2015-11-08 15:32:53 +0900

    fix FileClip checks hidden item on append other item bug

--- plugins/FileClip/loader.js
+++ plugins/FileClip/loader.js
@@ -1,13 +1,19 @@
 MapsGPX.plugin.FileClip = {
   callback: function(params) {
+    var $ul;
+
     if ( ! this.context['SidePanelControl'] ) {
       console.log('The plugin "FileClip" requires "SidePanelControl"');
       return;
     }
 
-    if ( ! this.context['FileClip'] ) {
+    if ( this.context['FileClip'] ) {
+      $ul = this.context['FileClip'].getElementsByTagName('ul').item(0);
+    } else {
       this.context['FileClip'] = document.createElement('div');
       this.context['SidePanelControl'].getElementDrawer().appendChild(this.context['FileClip']);
+      $ul = document.createElement('ul');
+      this.context['FileClip'].appendChild($ul);
     }
 
     this.addFilter('FileClip', 'onAppearOverlayShow', (function(overlay, key) {
@@ -25,44 +31,32 @@ MapsGPX.plugin.FileClip = {
     }).bind(this));
 
     this.register('onAddGPX', function(key) {
-      var i, l, $cb, text, contents, $label, $li,
-        $ul = document.createElement('ul'),
-        regexp = new RegExp('([^/]+)$'),
-        keys = this.getKeysOfGPX();
-      for ( i = 0, l = keys.length; i < l; ++i ) {
-        $cb = document.createElement('input');
-        $cb.setAttribute('type', 'checkbox');
-        $cb.setAttribute('value', keys[i]);
-        $cb.setAttribute('checked', 'checked');
-        google.maps.event.addDomListener($cb, 'change', (function(ev) {
-          if ( ev.target.checked ) {
-            this.showOverlayGpxs(ev.target.value);
-          } else {
-            this.hideOverlayGpxs(ev.target.value);
-          }
-        }).bind(this));
-
-        text = keys[i];
-        if ( regexp.test(text) ) {
-          text = RegExp.$1;
+      var $cb, $label, $li, name;
+      $cb = document.createElement('input');
+      $cb.setAttribute('type', 'checkbox');
+      $cb.setAttribute('value', key);
+      $cb.checked = true;
+      google.maps.event.addDomListener($cb, 'change', (function(ev) {
+        if ( ev.target.checked ) {
+          this.showOverlayGpxs(ev.target.value);
+        } else {
+          this.hideOverlayGpxs(ev.target.value);
         }
-        contents = document.createTextNode(text);
+      }).bind(this));
 
-        $label = document.createElement('label');
-        $label.appendChild($cb);
-        $label.appendChild(contents);
-
-        $li = document.createElement('li');
-        $li.appendChild($label);
-
-        $ul.appendChild($li);
+      name = key;
+      if ( new RegExp('([^/]+)$').test(name) ) {
+        name = RegExp.$1;
       }
 
-      // remove all children
-      while (this.context['FileClip'].firstChild) {
-        this.context['FileClip'].removeChild(this.context['FileClip'].firstChild);
-      }
-      this.context['FileClip'].appendChild($ul);
+      $label = document.createElement('label');
+      $label.appendChild($cb);
+      $label.appendChild(document.createTextNode(name));
+
+      $li = document.createElement('li');
+      $li.appendChild($label);
+
+      this.context['FileClip'].getElementsByTagName('ul').item(0).appendChild($li);
     });
   }
 };
