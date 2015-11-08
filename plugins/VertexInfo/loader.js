d2e2e313681e9f78c43ef63770b73e5724cf55bd
WATANABE Hiroaki <hwat@mac.com>
2015-11-08 14:51:50 +0900

    split class MercatorProjection

--- plugins/VertexInfo/loader.js
+++ plugins/VertexInfo/loader.js
@@ -1,55 +1,8 @@
 MapsGPX.plugin.VertexInfo = {
-  latlngFromOriginByPixel: function(/*LatLng*/origin, /*pixel*/delta_x, /*pixel*/delta_y, zoom) {
-    // Original code by Google
-    // https://developers.google.com/maps/documentation/javascript/examples/map-coordinates
-    var TILE_SIZE = 256;
-    function bound(value, opt_min, opt_max) {
-      if (opt_min != null) value = Math.max(value, opt_min);
-      if (opt_max != null) value = Math.min(value, opt_max);
-      return value;
-    }
-    function degreesToRadians(deg){
-      return deg * (Math.PI / 180);
-    }
-    function radiansToDegrees(rad){
-      return rad / (Math.PI / 180);
-    }
-    function MercatorProjection() {
-      this.pixelOrigin_ = new google.maps.Point(TILE_SIZE / 2, TILE_SIZE / 2);
-      this.pixelsPerLonDegree_ = TILE_SIZE / 360;
-      this.pixelsPerLonRadian_ = TILE_SIZE / (2 * Math.PI);
-    }
-    MercatorProjection.prototype.fromLatLngToPoint = function(latLng, opt_point){
-      var point = opt_point || new google.maps.Point(0, 0);
-      point.x = this.pixelOrigin_.x + latLng.lng() * this.pixelsPerLonDegree_;
-
-      // Truncating to 0.9999 effectively limits latitude to 89.189. This is
-      // about a third of a tile past the edge of the world tile.
-      var siny = bound(Math.sin(degreesToRadians(latLng.lat())), -0.9999, 0.9999);
-      point.y = this.pixelOrigin_.y + 0.5 * Math.log((1 + siny) / (1 - siny)) * -this.pixelsPerLonRadian_;
-      return point;
-    };
-    MercatorProjection.prototype.fromPointToLatLng = function(point){
-      var lng = (point.x - this.pixelOrigin_.x) / this.pixelsPerLonDegree_;
-      var latRadians = (point.y - this.pixelOrigin_.y) / -this.pixelsPerLonRadian_;
-      var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) - Math.PI / 2);
-      return new google.maps.LatLng(lat, lng);
-    };
-
-    var num_tiles         = 1 << zoom;
-    var projection        = new MercatorProjection();
-    var world_coordinate  = projection.fromLatLngToPoint(origin);
-    var pixel_coordinate  = new google.maps.Point(
-                              world_coordinate.x * num_tiles,
-                              world_coordinate.y * num_tiles);
-    var tile_coordinate   = new google.maps.Point(
-                              Math.floor(pixel_coordinate.x / TILE_SIZE),
-                              Math.floor(pixel_coordinate.y / TILE_SIZE));
-    var x = world_coordinate.x + (delta_x / num_tiles);
-    var y = world_coordinate.y + (delta_y / num_tiles);
-    return projection.fromPointToLatLng(new google.maps.Point(x, y));
-  },
-  indexOfVertexNearestLatlng: function(/*MVCArray*/path, latlng, zoom) {
+  bundles: [
+    'MercatorProjection.js'
+  ],
+  indexOfVertexNearestLatlng: function(projection, /*MVCArray*/path, latlng, zoom) {
     // looking for a pair of points that make a segment which latlng rides,
     // it is the smallest angle of two segments, (path[p] - path[p+1]) and (latlng - path[p+1])
     var EARTH_ROUND = 6378137,
@@ -63,8 +16,8 @@ MapsGPX.plugin.VertexInfo = {
       b = new google.maps.LatLngBounds();
       b.extend(path.getAt(i));
       b.extend(path.getAt(i+1));
-      b.extend(MapsGPX.plugin.VertexInfo.latlngFromOriginByPixel(b.getNorthEast(),  margin, -margin, zoom));
-      b.extend(MapsGPX.plugin.VertexInfo.latlngFromOriginByPixel(b.getSouthWest(), -margin,  margin, zoom));
+      b.extend(MercatorProjection.latlngFromOriginByPixel(projection, b.getNorthEast(),  margin, -margin, zoom));
+      b.extend(MercatorProjection.latlngFromOriginByPixel(projection, b.getSouthWest(), -margin,  margin, zoom));
       if ( b.contains(latlng) ) {
         p0   = path.getAt(i),
         p1   = latlng,
@@ -90,12 +43,17 @@ MapsGPX.plugin.VertexInfo = {
     return minindex;
   },
   callback: function(params) {
+    var projection = new MercatorProjection();
+    this.context['VertexInfo'] = {
+      projection: projection
+    };
     // add hook points
     this.hook['onVertexInfo'] = this.hook['onVertexInfo'] || [];
     // 
     this.register('onCreatePolyline', (function(polyline) {
       polyline.addListener('click', (function(mouseevent) {
         var idx = MapsGPX.plugin.VertexInfo.indexOfVertexNearestLatlng(
+                    this.app.context['VertexInfo'].projection,
                     this.polyline.getPath(), mouseevent.latLng, this.polyline.getMap().getZoom());
         if ( this.app.hook['onVertexInfo'].length != 0 ) {
           this.app.applyHook('onVertexInfo', polyline, idx, mouseevent);
@@ -104,24 +62,22 @@ MapsGPX.plugin.VertexInfo = {
         }
       }).bind({app: this, polyline: polyline}));
     }).bind(this));
-
   },
   _handlerHookOnClickPolylineDefault: function(polyline, index, mouseevent) {
     var wpt, content;
     if ( 0 <= index && polyline.isTrk() ) {
       wpt = polyline.getSource()[index];
-      content = '#'+ index +'<br/>lat='+ wpt.lat +'<br/>lon='+ wpt.lon;
+      content = ['#', index, '<br/>lat=', wpt.lat, '<br/>lon=', wpt.lon];
       if ( wpt.ele ) {
-        content = content + '<br/>ele='+ wpt.ele;
+        content.push('<br/>ele='+ wpt.ele);
       }
       if ( wpt.time ) {
-        content = content + '<br/>time='+ wpt.time;
+        content.push('<br/>time='+ wpt.time);
       }
       new google.maps.InfoWindow({
-        content: content,
+        content: content.join(''),
         position: polyline.getPath().getAt(index)
         }).open(polyline.getMap());
     }
-
   }
 };
